9b33ac7d5f95f52696b55206288b86c644e42c96
David Kotlirevsky <david.kotlirevsky@gmail.com>
2023-12-21 16:19:36 -0300

    new scenarios

--- double_hanoi/double_hanoi.py
+++ double_hanoi/double_hanoi.py
@@ -1,17 +1,36 @@
-def check_is_lower_in_left(disk, left):
+def check_is_lower(disk, left):
     if disk in left:
         return left
     max_val = max(left)
     if disk < max_val:
         for idx, val in enumerate(left):
             next_idx = idx + 1
-            if next_idx < len(left):
-                next_val = left[next_idx]
-            else:
-                next_val = (disk - 1)
+            next_val = obtain_next_val(disk, left, next_idx)
             if val > disk >= next_val:
                 left.insert(idx+1, disk)
     return left
+
+def obtain_next_val(disk, left, next_idx):
+    if next_idx < len(left):
+        next_val = left[next_idx]
+    else:
+        next_val = (disk - 1)
+    return next_val
+
+def check_is_higher(disk, right):
+    if disk in right:
+        return right
+    min_val = min(right)
+    if disk > min_val:
+        for idx, val in enumerate(right):
+            next_idx = idx + 1
+            if next_idx < len(right):
+                next_val = right[next_idx]
+            else:
+                next_val = (disk + 1)
+            if val < disk < next_val:
+                right.insert(idx + 1, disk)
+    return right
 
 
 def solution(list_of_disks, leftIncreasingValues, rightDecreasingValues):
@@ -19,11 +38,9 @@ def solution(list_of_disks, leftIncreasingValues, rightDecreasingValues):
     right = [rightDecreasingValues]
     for disk in list_of_disks:
         original_left = len(left)
-        left = check_is_lower_in_left(disk, left)
+        left = check_is_lower(disk, left)
         if original_left == len(left):
-            if disk > right[-1]:
-                right.append(disk)
-
+            right = check_is_higher(disk, right)
     return len(left) + len(right) - 2
 
 
--- double_hanoi/test_double_hanoi.py
+++ double_hanoi/test_double_hanoi.py
@@ -1,8 +1,9 @@
 import pytest
 
-from double_hanoi import solution, check_is_lower_in_left
+from double_hanoi import solution, check_is_lower, check_is_higher
 
 test_data = [
+    ([5, 2, 5, 2], 8, 1, 4),
     ([1, 4, 5, 5], 6, 4, 4),
     ([2, 3, 3, 4], 3, 1, 3),
 
@@ -10,11 +11,9 @@ test_data = [
 
 params = "list_of_disks, leftIncreasing, rightDecreasing, expected"
 
-
 @pytest.mark.parametrize(params, test_data)
 def test_solution(list_of_disks, leftIncreasing, rightDecreasing, expected):
     assert solution(list_of_disks, leftIncreasing, rightDecreasing) == expected
-
 
 test_data2 = [
     ([4, 2], 3, [4, 3, 2]),
@@ -24,8 +23,18 @@ test_data2 = [
 
 params2 = "aList, value, expected"
 
-
 @pytest.mark.parametrize(params2, test_data2)
-def test_check_is_lower_in_left(aList, value, expected):
-    result = check_is_lower_in_left(value, aList)
+def test_check_is_lower(aList, value, expected):
+    result = check_is_lower(value, aList)
+    assert result == expected
+
+test_data3 = [
+    ([4, 7], 6, [4, 6, 7]),
+    ([3, 4], 5, [3, 4, 5]),
+    ([5, 6, 7], 7, [5, 6, 7]),
+]
+
+@pytest.mark.parametrize(params2, test_data3)
+def test_check_is_higher(aList, value, expected):
+    result = check_is_higher(value, aList)
     assert result == expected
