05ca130150b149e0bee45c048622f593a201a3ee
David Kotlirevsky <david.kotlirevsky@gmail.com>
2024-02-28 10:53:42 -0300

    pangrams

---
+++ codewars/pangrams/test_example.py
@@ -0,0 +1,15 @@
+import pytest
+
+from codewars.pangrams.example import solution
+
+test_data =[
+    (1, 1, 2),
+    (3, 3, 6),
+    (3, 4, 7),
+]
+params = "p1, p2, expected"
+
+
+@pytest.mark.parametrize(params, test_data)
+def test_solution(p1, p2, expected):
+    assert solution(p1, p2) == expected
