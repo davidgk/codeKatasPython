6a85c6ca75fac35a7665b850fae068f9284a1d58
David Kotlirevsky <david.kotlirevsky@gmail.com>
2024-03-04 12:00:29 -0300

    solve pangram

--- codewars/pangrams/example.py
+++ codewars/pangrams/example.py
@@ -1,3 +1,43 @@
-def solution(a, b):
-    return a + b
+from functools import reduce
 
+
+def filter_data(acum,  value):
+    if value.isalpha():
+        if value in acum:
+            acum[value] = acum[value] + 1
+        else:
+            acum[value] = 1
+    return acum
+    # try:
+    #     pass
+    # except Exception as ex:
+    #     print(ex)
+    #     return acum
+
+def solution(aPhrase):
+    initial = list(str(aPhrase).strip().lower())
+    if len(initial) == 0:
+        return False
+    else:
+        return len(reduce(filter_data, initial, {})) == 26
+
+# otjher interesting solutions
+
+import string
+
+def is_pangram(s):
+    s = s.lower()
+    for token in string.ascii_lowercase:
+        if token not in s:
+            return False
+    return True
+
+
+def is_pangram2(s):
+    return set(string.ascii_lowercase).issubset(s.lower())
+
+
+
+def is_pangram3(input_string):
+    """Returns False if the input string is not a pangram else returns True"""
+    return False if [char for char in string.ascii_lowercase if not char in input_string.lower()] else True
--- codewars/pangrams/test_example.py
+++ codewars/pangrams/test_example.py
@@ -3,13 +3,17 @@ import pytest
 from codewars.pangrams.example import solution
 
 test_data =[
-    (1, 1, 2),
-    (3, 3, 6),
-    (3, 4, 7),
+    ("; 5 abc d", False),
+    (1, False),
+    ("; 5 aBC d", False),
+    ("ab cd ef;  gh ij k", False),
+    ("ab cde fgh ij klmnopq rstuv. wxyz", True),
+    ("ab cde fgh ij klMnopq 12345 rstuv. Wxyz", True),
+    ("ab cde fgh ij klMnopq 12345 rstuv. Wxyz ABCD E", True),
 ]
-params = "p1, p2, expected"
+params = "p1, expected"
 
 
 @pytest.mark.parametrize(params, test_data)
-def test_solution(p1, p2, expected):
-    assert solution(p1, p2) == expected
+def test_solution(p1, expected):
+     assert solution(p1) == expected
