9192566c8c1c0e04a676ef56b55dabc1c897a428
David Kotlirevsky <david.kotlirevsky@gmail.com>
2024-11-29 15:05:33 -0300

    enhance creator

--- codewars/codewars.sh
+++ codewars/codewars.sh
@@ -1,7 +1,10 @@
 function replace_kata_name(){
    sed -i '' "s/blank/$1/g" project.sh;
    sed -i '' "s/blank/$1/g" test_example.py;
+   sed -i '' "s/solution/$1/g" example.py;
+   sed -i '' "s/solution/$1/g" test_example.py;
    uppercase_replacement=$(echo "$1" | tr '[:lower:]' '[:upper:]')
    sed -i '' "s/description/$uppercase_replacement/g" README.md;
+   cd ../..; gadd .; git commit -m  "$1"
 }
 
